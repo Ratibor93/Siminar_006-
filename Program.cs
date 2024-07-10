9c995ec3d6c8e6716a3b19639ba35b28c546227d
Ratibor93 <ratibor.semenov.93@gmail.com>
2024-07-10 23:36:30 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод исходного массива строк
+        Console.WriteLine("Введите элементы массива через пробел:");
+        string input = Console.ReadLine();
+        string[] array = input.Split(' ');
+
+        // Формирование нового массива из строк, длина которых <= 3 символов
+        string[] newArray = FilterShortStrings(array);
+
+        // Вывод нового массива
+        Console.WriteLine("Новый массив:");
+        foreach (string str in newArray)
+        {
+            Console.Write($"{str} ");
+        }
+    }
+
+    static string[] FilterShortStrings(string[] array)
+    {
+        // Подсчет количества строк, длина которых <= 3 символов
+        int count = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Создание нового массива нужного размера
+        string[] newArray = new string[count];
+
+        // Заполнение нового массива подходящими строками
+        int index = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                newArray[index] = str;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
+}
